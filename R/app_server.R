805201826789162e6cab39e8356f8ba0e13a9ec2
wayneliuq <wayneliuq@gmail.com>
2022-04-19 16:13:57 -0400

    modularize regression (WIP)

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,9 @@
 #' @noRd
 app_server <- function( input, output, session ) {
 
+  #### modules ####
+  mod_regression_server("regression_1")
+
   #### final output plot ####
   # this is the ggplot2 function which will render the final plot
 
@@ -308,8 +311,8 @@ app_server <- function( input, output, session ) {
   data_do <- reactive({
 
     data_get() |> select(
-      x = input$xvar |> tryCatch(error = function(e) NULL),
-      y = input$yvar |> tryCatch(error = function(e) NULL),
+      x = input$xvar |> tryCatch(error = function(e) 0),
+      y = input$yvar |> tryCatch(error = function(e) 0),
       colorNumeric = color_numeric_var_formdf(),
       colorFactor = color_factor_var_formdf(),
       facetHFactor = facet_hvar_formdf(),
@@ -318,25 +321,6 @@ app_server <- function( input, output, session ) {
 
   })
 
-  ## regression formula
-  regr_formula <- reactive({
-    if (any(grepl("Factor", colnames(data_do())))) {
-      paste0(
-        "y ~ x + ",
-        (grep("Factor", colnames(data_do()), value = T) |> paste(collapse = " + "))) |> as.formula()
-    } else {
-      y ~ x
-    }
-  })
-
-  ## regression model
-  regr_model <- reactive({
-
-    if (input$regression_conty == "lm") {
-      lm(regr_formula(), data = data_do())
-    }
-
-  })
 
   ## get coordinates for drawing
   data_regr <- reactive({
